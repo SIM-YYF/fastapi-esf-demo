2ebf2df48b7988fc529bc52be925ca5747070074
yuanwenfei <yuanwenfei@k2data.com.cn>
2019-12-20 19:26:15 +0800

    xxx

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -1,10 +1,10 @@
 # SQLAlchemy specific code, as with any other app
 import databases
 import sqlalchemy
+from sqlalchemy import Integer, String
 from sqlalchemy_utils import database_exists, create_database
 
-
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5433/esf"
+DATABASE_URL = "postgresql://postgres:postgres@localhost:5433/test_esf"
 
 if not database_exists(DATABASE_URL):
     create_database(DATABASE_URL)
@@ -16,16 +16,19 @@ users = sqlalchemy.Table(
     "esf_user",
     metadata,
     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column("text", sqlalchemy.String),
-    sqlalchemy.Column("completed", sqlalchemy.Boolean),
+    sqlalchemy.Column("role_id", sqlalchemy.ForeignKey('esf_roles.id', ondelete='CASCADE')),  # 级联删除
+    sqlalchemy.Column("name", sqlalchemy.String),
+    sqlalchemy.Column("full_name", sqlalchemy.String)
+
 )
 
 roles = sqlalchemy.Table(
     "esf_roles",
     metadata,
     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column("text", sqlalchemy.String),
-    sqlalchemy.Column("completed", sqlalchemy.Boolean),
+    sqlalchemy.Column('user_id', sqlalchemy.ForeignKey('esf_user.id', ondelete='CASCADE')),  # 级联删除
+    sqlalchemy.Column("name", sqlalchemy.String),
+    sqlalchemy.Column("display", sqlalchemy.String),
 )
 
 engine = sqlalchemy.create_engine(
